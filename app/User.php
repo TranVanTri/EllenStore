c5609a4c5383b9a4964c0200d900df21dbdb4087
tomiot1097 <tomiot1097@gmail.com>
2018-12-06 22:05:17 +0700

    Update User.php

--- app/User.php
+++ app/User.php
@@ -10,10 +10,21 @@ class User extends Authenticatable
 {
     public $timestamps = false;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
     protected $fillable = [
         'name', 'email', 'password','provider','provider_id'
     ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
     protected $hidden = [
         'password', 'remember_token',
     ];
