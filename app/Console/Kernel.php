56f6053db9221791e9f59f08400eeed649f7a6cf
TranVanTri <tvtri1997@gmail.com>
2018-12-17 20:26:16 +0700

    chinh check date km

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\checkDatePromotion;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        checkDatePromotion::class,
     ];
 
     /**
@@ -25,7 +26,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')
-        //          ->hourly();
+        //          ->everyMinute();
+
+        $schedule->command('check:datepromotion')
+                 ->dailyAt('23:59:59');
+        
     }
 
     /**
