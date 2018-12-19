61071cd0669b3e0b0c54fcf5a5128032ebba81e5
TranVanTri <tvtri1997@gmail.com>
2018-12-19 22:03:08 +0700

    Sửa menu left

--- app/Http/Controllers/UserController/BeforeCartController.php
+++ app/Http/Controllers/UserController/BeforeCartController.php
@@ -11,22 +11,26 @@ use Illuminate\Database\QueryException;
 use App\Http\Controllers\Controller;
 class BeforeCartController extends Controller
 {
+
+    public function __construct(){
+        $cateGroup = CategoryGroup::where('enable',1)->get();
+        view()->share('cateGroup', $cateGroup);
+    }
+
     public function getViewProduct($id)
     {	
     	// need to change the id of the idCategoryProduct
-    	$cateGroup = DB::table('category_group')->where('enable',1)->get();
-    	$cateProduct = DB::table('category_product')->where('enable',1)->get();
-    	$products = DB::table('product')->where([['idCategoryProduct','=',$id],['enable','=', 1]])->get();
+    	
+    	$products = Product::where([['idCategoryProduct','=',$id],['enable','=', 1]])->get();
 
         // get the Name of the Category_Product
-    	$dirName = DB::table('category_product')->where('id',$id)->get();
+    	$dirName = CategoryProduct::where('id',$id)->get();
 
-    	return view('user/tatcasanpham',compact('products','cateGroup',
-    		'cateProduct','dirName'));
+    	return view('user/tatcasanpham',compact('products', 'dirName'));
     }
 
     public function viewDetailProduct($id){
-    	$product = DB::table('product')->where('id',$id)->get();
+    	$product = Product::where('id',$id)->get();
     	return view('user/chitietsanpham',compact('product'));
     }
 
--- resources/views/user/core/categoryProduct.blade.php
+++ resources/views/user/core/categoryProduct.blade.php
@@ -1,18 +1,20 @@
 <ul>
 	@foreach($cateGroup as $groupChild)
-	<li>
-		<div class="dropdown-cate-pro">
-			<i class="fa fa-minus-square-o" aria-hidden="true">
-			<a href="#"></i>{{$groupChild->name}}</a>
-		</div>
-		
-		<ul>
-			@foreach($cateProduct as $cateProductChild)
-				@if($groupChild->id == $cateProductChild->idCategoryGroup)
-					<li><a href="{{route('tatcasanpham', ['id' => $cateProductChild->id])}}"><span>-&nbsp;</span>{{$cateProductChild->name}}</a></li>
-				@endif
-			@endforeach
-		</ul>
-	</li>
+		@if(count($groupChild->category_product) && count($groupChild->product))
+			<li>
+				<div class="dropdown-cate-pro">
+					<i class="fa fa-minus-square-o" aria-hidden="true">
+					<a href="#"></i>{{$groupChild->name}}</a>
+				</div>
+				
+				<ul>
+					@foreach($groupChild->category_product as $cateProductChild)
+						@if(count($cateProductChild->product))		
+							<li><a href="{{route('tatcasanpham', ['id' => $cateProductChild->id])}}"><span>-&nbsp;</span>{{$cateProductChild->name}}</a></li>
+						@endif
+					@endforeach
+				</ul>
+			</li>
+		@endif
 	@endforeach					
 </ul>
